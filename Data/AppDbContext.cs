d6ada5f1bea243743f09ef84958716b74072e487
leticia.ssilva <leticia.saraiva013@gmail.com>
2025-05-16 09:09:29 -0300

    Adicionar arquivos de projeto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using consoleDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace consoleDB.Data
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly string _connectionString;
+        public DbSet<Product> Products { get; set; }
+
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        public AppDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using consoleDB.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var connectionString = config.GetConnectionString("WiredBrain");
+
+using var context = new AppDbContext(connectionString);
+
+var lstProducts = context.Products.ToList();
+
+
+Console.ReadKey();
