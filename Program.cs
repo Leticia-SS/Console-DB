dba183ddead46ffa4757b928f799e6718b2ce5db
leticia.ssilva <leticia.saraiva013@gmail.com>
2025-05-21 09:10:42 -0300

    connecion and reading from database with ADONET

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using consoleDB.Data;
+using consoleDB.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
@@ -8,13 +9,56 @@ var config = new ConfigurationBuilder()
 
 var connectionString = config.GetConnectionString("WiredBrain");
 
-using var context = new AppDbContext(connectionString);
-
-var lstProducts = context.Products.ToList();
-
-foreach (var product in lstProducts)
+GetProductADONET();
+void GetProductADONET()
 {
-    Console.WriteLine($"[{product.Id}] {product.Name} - {product.Price}");
+    var products = new List<Product>();
+
+    using var connection = new SqlConnection(connectionString); // Se fosse oracle ou posgre teria que baixqar extensão
+    connection.Open();
+
+    string query = @"SELECT Id, Name, Description, ShortDescription, Price, ImageFile, Created, Category FROM Products";
+    
+    using var command = new SqlCommand(query, connection);
+
+    using var reader = command.ExecuteReader();
+
+    while (reader.Read())
+    {
+        var product = new Product
+        {
+            Id = reader.GetInt32(0),
+            Name = reader.GetString(1),
+            Description = reader.GetString(2),
+            ShortDescription = reader.GetString(3),
+            Price = reader.GetDecimal(4),
+            ImageFile = reader.IsDBNull(5) ? null: reader.GetString(5),
+            Created = reader.GetDateTime(6),
+            Category = reader.GetString(7),
+        };
+
+        products.Add(product);
+    }
+
+    foreach (var product in products)
+    {
+        Console.WriteLine($"[{product.Id}] {product.Name} - {product.Price}");
+    }
 }
+
+
+void GetProductEF()
+{
+    using var context = new AppDbContext(connectionString);
+
+    var lstProducts = context.Products.ToList();
+
+    foreach (var product in lstProducts)
+    {
+        Console.WriteLine($"[{product.Id}] {product.Name} - {product.Price}");
+    }
+
+}
+
 
 Console.ReadKey();
